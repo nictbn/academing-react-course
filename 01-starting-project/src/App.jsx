0ec307d709ddbfabc3f09e8b5b10c3da9642b7db
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-30 01:03:32 +0300

    Introduces the concept of props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -20,11 +21,27 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description="The core UI building block." image={componentsImg} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
