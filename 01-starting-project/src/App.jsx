aaa02175330077c0b2df4d1c14ed6a650614fa71
Nicu Taban <nictbndeveloper@gmail.com>
2024-08-30 01:11:35 +0300

    Simplifies props passing

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -41,25 +41,17 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
-            <CoreConcept 
-             title={CORE_CONCEPTS[0].title}
-             description={CORE_CONCEPTS[0].description}
-             image={CORE_CONCEPTS[0].image} 
+            <CoreConcept
+            {...CORE_CONCEPTS[0]}
             />
-            <CoreConcept 
-             title={CORE_CONCEPTS[1].title}
-             description={CORE_CONCEPTS[1].description}
-             image={CORE_CONCEPTS[1].image} 
+            <CoreConcept
+            {...CORE_CONCEPTS[1]}
             />
-            <CoreConcept 
-             title={CORE_CONCEPTS[2].title}
-             description={CORE_CONCEPTS[2].description}
-             image={CORE_CONCEPTS[2].image} 
+            <CoreConcept
+            {...CORE_CONCEPTS[2]}
             />
-            <CoreConcept 
-             title={CORE_CONCEPTS[3].title}
-             description={CORE_CONCEPTS[3].description}
-             image={CORE_CONCEPTS[3].image} 
+            <CoreConcept
+            {...CORE_CONCEPTS[3]}
             />
           </ul>
         </section>
