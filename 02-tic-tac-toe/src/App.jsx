d1a955c9e399bdf0f9a476df3596cedd4ffcf9c6
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-05 23:21:45 +0200

    Adds the gameboard component

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from './components/Player.jsx'
+import GameBoard from './components/GameBoard.jsx'
 
 function App() {
   
@@ -10,7 +11,7 @@ function App() {
           <Player initialName="Player 1" symbol="X"/>
           <Player initialName="Player 2" symbol="O"/>
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
     </main>
   )
