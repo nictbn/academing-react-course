5e7f048b382324c232bffbc507d61fd8df81d963
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-08 23:05:06 +0200

    Populates the log component

--- 02-tic-tac-toe/src/components/Log.jsx
+++ 02-tic-tac-toe/src/components/Log.jsx
@@ -1,7 +1,7 @@
-export default function Log() {
+export default function Log({ turns }) {
     return (
         <ol id="log">
-            
+            {turns.map(turn => <li key={`${turn.square.row}${turn.square.column}`}>{turn.player} selected {turn.square.row}, {turn.square.column}</li>)}
         </ol>
     )
 }
