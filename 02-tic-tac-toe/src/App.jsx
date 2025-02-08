7d1cdad7e0f8f2a83f5cf8503a329c889de81e22
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-08 22:59:50 +0200

    Derives state; Adds a log component

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 
 import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx'
+import Log from './components/Log.jsx';
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, columnIndex) {
     setActivePlayer((currentlyActivePlayer) => currentlyActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns((previousTurns) => {
+      let currentPlayer = 'X';
+      if (previousTurns.length > 0 && previousTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [{square: {row: rowIndex, column: columnIndex}, player: currentPlayer}, ...previousTurns]
+      return updatedTurns;
+    });
   }
 
   return (
@@ -17,8 +27,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
+      <Log/>
     </main>
   )
 }
