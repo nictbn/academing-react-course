1b6ad78567692775b4b0e4d59383cdb85d7654c8
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-09 22:43:24 +0200

    Fixes duration bug

--- 03-investment-calculator/src/App.jsx
+++ 03-investment-calculator/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     expectedReturn: 6,
     duration: 10,
   });
+  const inputIsValid = userInput.duration >= 1
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((previousUserInput) => {
@@ -25,7 +26,8 @@ function App() {
     <>
         <Header />
         <UserInput userInput={userInput} onChange={handleChange} />
-        <Results input={userInput} />
+        {inputIsValid && <Results input={userInput} />}
+        {!inputIsValid && <p className='center'>Please enter a duration greater than zero</p>}
     </>
   )
 }
