22a69ab80d8780fcaf522ecac154b1722a2e7d44
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-17 22:02:58 +0200

    Recap two way binding and use state

--- 06-refs-portals/src/components/Player.jsx
+++ 06-refs-portals/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChage(event) {
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChage} value={enteredPlayerName}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
