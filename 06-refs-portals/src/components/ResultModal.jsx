9ad474c1dcfd6ebd5fa47efaf893e1b5b193a286
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-20 23:15:22 +0200

    Use a portal to render the modal in a different place in the dom

--- 06-refs-portals/src/components/ResultModal.jsx
+++ 06-refs-portals/src/components/ResultModal.jsx
@@ -1,4 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
+
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialog = useRef();
     useImperativeHandle(ref, () => {
@@ -13,7 +15,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
     const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
+    return createPortal(<dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>You lost</h2>}
         {!userLost && <h2>Your score: {score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
@@ -21,7 +23,9 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>;
+    </dialog>, 
+    document.getElementById('modal')
+);
 });
 
 export default ResultModal;
