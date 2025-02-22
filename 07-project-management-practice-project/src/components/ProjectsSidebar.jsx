492ae85effc3f3173796c9ff77152cdbf5048c30
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-22 20:41:44 +0200

    Adds barebones sidebar

---
+++ 07-project-management-practice-project/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,17 @@
+export default function ProjectsSidebar() {
+    return (
+        <>
+            <aside>
+                <h2>Your projects</h2>
+            </aside>
+            <div>
+                <button>
+                    + Add project
+                </button>
+            </div>
+            <ul>
+
+            </ul>
+        </>
+    )
+}
