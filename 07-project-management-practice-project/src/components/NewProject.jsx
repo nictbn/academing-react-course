e6b8bb7686a08d16e2419aaf9c7e40e35f80f293
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-23 01:01:38 +0200

    Styles the modal and handles pressing the cancel button

--- 07-project-management-practice-project/src/components/NewProject.jsx
+++ 07-project-management-practice-project/src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import Input from "./Input.jsx";
 import Modal from './Modal.jsx';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
     const titleRef = useRef();
     const descriptionRef = useRef();
     const dueDateRef = useRef();
@@ -29,13 +29,13 @@ export default function NewProject({ onAdd }) {
     return (
         <>
             <Modal ref={modalRef} buttonCaption="Close">
-                <h2>Invalid input</h2>
-                <p>Oops... Looks like you forgot to enter a value.</p>
-                <p>Please make sure you provide a valid value for every input field.</p>
+                <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid input</h2>
+                <p className='text-stone-600 mb-4'>Oops... Looks like you forgot to enter a value.</p>
+                <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
             </Modal>
             <div className="w-[35rem] mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4">
-                    <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                    <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
                     <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
                 </menu>
                 <div>
