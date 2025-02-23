7d5624fcd0394240ac4b59e3d56f7c0b9780a1de
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-23 15:54:30 +0200

    Consuming context

--- 08-advanced-state-management/src/components/Cart.jsx
+++ 08-advanced-state-management/src/components/Cart.jsx
@@ -1,5 +1,9 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
-  const totalPrice = items.reduce(
+import { useContext } from 'react';
+
+import { CartContext } from "../store/shopping-cart-context.jsx";
+export default function Cart({ onUpdateItemQuantity }) {
+  const cartCtx = useContext(CartContext);
+  const totalPrice = cartCtx.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
@@ -7,10 +11,10 @@ export default function Cart({ items, onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
+      {cartCtx.length === 0 && <p>No items in cart!</p>}
+      {cartCtx.length > 0 && (
         <ul id="cart-items">
-          {items.map((item) => {
+          {cartCtx.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
