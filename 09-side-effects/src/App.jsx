1c7a843ea42905e33e36d4571223a3e582db86aa
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-24 21:37:42 +0200

    Use useEffect to control the modal

--- 09-side-effects/src/App.jsx
+++ 09-side-effects/src/App.jsx
@@ -13,7 +13,7 @@ const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place) => plac
 
 function App() {
   // can also be placed here
-  const modal = useRef();
+  const[modalIsOpen, setModalIsOpen] = useState(false);
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
@@ -26,12 +26,12 @@ function App() {
   
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -53,14 +53,14 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
   }
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
