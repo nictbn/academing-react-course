aa2d0feaa0b2a1520dde07b9612f21cf2a8619d9
Nicu Taban <nictbndeveloper@gmail.com>
2025-02-26 00:16:06 +0200

    Improve performance by re rendering only the progress bar component

--- 09-side-effects/src/components/DeleteConfirmation.jsx
+++ 09-side-effects/src/components/DeleteConfirmation.jsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar.jsx";
 
 const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log('INTERVAL');
-      setRemainingTime((previousTime) => previousTime - 10);
-    }, 10)
-
-    return () => {
-      clearInterval(interval);
-    }
-  }, [])
-  
   useEffect(() => {
     const timer = setTimeout(() => {
       console.log('SETTING TIMER')
@@ -37,7 +26,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER}/>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
