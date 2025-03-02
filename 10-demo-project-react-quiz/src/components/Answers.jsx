19d11aaea3c824eea4af5a1440acd7a4dca3b163
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-02 18:06:58 +0200

    Adds question component

---
+++ 10-demo-project-react-quiz/src/components/Answers.jsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+
+export default function Answers({answers, selectedAnswer, answerState, onSelect}) {
+    const shuffledAnswers = useRef();
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...answers];
+        shuffledAnswers.current.sort((a, b) => Math.random - 0.5);
+    }
+    return (
+        <ul id="answers">
+            {shuffledAnswers.current.map((answer) => {
+                let cssClass = '';
+                const isSelected = selectedAnswer === answer;
+                if (answerState === 'answered' && isSelected) {
+                    cssClass = 'selected'
+                }
+                if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                    cssClass = answerState
+                }
+                return (
+                    <li key={answer} className='answer'>
+                        <button onClick={() => onSelect(answer)} className={cssClass}>{answer}</button>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
--- 10-demo-project-react-quiz/src/components/Quiz.jsx
+++ 10-demo-project-react-quiz/src/components/Quiz.jsx
@@ -1,10 +1,9 @@
-import { useState, useCallback, useRef } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
-import QuestionTimer from './QuestionTimer.jsx';
+import Question from './Question.jsx';
 
 export default function Quiz() {
-    const shuffledAnswers = useRef();
     const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
@@ -35,33 +34,17 @@ export default function Quiz() {
             <h2>Quiz Completed!</h2>
         </div>
     }
-    if (!shuffledAnswers.current) {
-        shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
-        shuffledAnswers.current.sort((a, b) => Math.random - 0.5);
-    }
     return (
-        <div id='quiz'>
-            <div id="question">
-                <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={() => handleSkipAnswer(null)} />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.current.map((answer) => {
-                        let cssClass = '';
-                        const isSelected = userAnswers[userAnswers.length - 1] === answer;
-                        if (answerState === 'answered' && isSelected) {
-                            cssClass = 'selected'
-                        }
-                        if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
-                            cssClass = answerState
-                        }
-                        return (
-                            <li key={answer} className='answer'>
-                                <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>{answer}</button>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
+        <div id='quiz'>   
+            <Question
+                    key={activeQuestionIndex}
+                    questionText={QUESTIONS[activeQuestionIndex].text} 
+                    answers={QUESTIONS[activeQuestionIndex].answers}
+                    answerState={answerState}
+                    selectedAnswer={userAnswers[userAnswers.length - 1]}
+                    onSelectAnswer={handleSelectAnswer}
+                    onSkipAnswer={handleSkipAnswer}
+                />
         </div>
     )
 }
