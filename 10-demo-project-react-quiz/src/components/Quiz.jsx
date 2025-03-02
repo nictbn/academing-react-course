e70899030e79985c5a8ae49e8210bcbb4fda8c7e
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-02 17:39:29 +0200

    Fix highlightn=ing

--- 10-demo-project-react-quiz/src/components/Quiz.jsx
+++ 10-demo-project-react-quiz/src/components/Quiz.jsx
@@ -1,9 +1,10 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useRef } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from './QuestionTimer.jsx';
 
 export default function Quiz() {
+    const shuffledAnswers = useRef();
     const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
@@ -34,16 +35,17 @@ export default function Quiz() {
             <h2>Quiz Completed!</h2>
         </div>
     }
-
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort((a, b) => Math.random - 0.5);
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+        shuffledAnswers.current.sort((a, b) => Math.random - 0.5);
+    }
     return (
         <div id='quiz'>
             <div id="question">
                 <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={() => handleSkipAnswer(null)} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map((answer) => {
+                    {shuffledAnswers.current.map((answer) => {
                         let cssClass = '';
                         const isSelected = userAnswers[userAnswers.length - 1] === answer;
                         if (answerState === 'answered' && isSelected) {
