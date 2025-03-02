96768305168637c432efeac2c320fa41dfcf9a97
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-02 16:33:55 +0200

    Adds the question timer

--- 10-demo-project-react-quiz/src/components/Quiz.jsx
+++ 10-demo-project-react-quiz/src/components/Quiz.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from './QuestionTimer.jsx';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((previousUserAnswers) => {
             return [...previousUserAnswers, selectedAnswer]
         })
-    }
+    }, []);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
@@ -26,6 +29,7 @@ export default function Quiz() {
     return (
         <div id='quiz'>
             <div id="question">
+                <QuestionTimer timeout={10000} onTimeout={() => handleSkipAnswer(null)} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => {
