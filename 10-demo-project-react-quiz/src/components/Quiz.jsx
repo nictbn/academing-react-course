7f156850ed8ade343a77424c69df2ca7d00cc645
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-02 16:04:09 +0200

    Randomize answers and display quiz complete screen

--- 10-demo-project-react-quiz/src/components/Quiz.jsx
+++ 10-demo-project-react-quiz/src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
@@ -11,12 +12,23 @@ export default function Quiz() {
         })
     }
 
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+    if (quizIsComplete) {
+        return <div id='summary'>
+            <img src={quizCompleteImg} alt="Trophy Icon" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort((a, b) => Math.random - 0.5);
     return (
         <div id='quiz'>
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+                    {shuffledAnswers.map((answer) => {
                         return (
                             <li key={answer} className='answer'>
                                 <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
