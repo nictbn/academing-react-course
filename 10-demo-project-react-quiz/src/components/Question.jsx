e200f2eac474719a533d7e6cda0a0e6873468443
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-03 22:02:16 +0200

    Sets different timers based on selected answer

--- 10-demo-project-react-quiz/src/components/Question.jsx
+++ 10-demo-project-react-quiz/src/components/Question.jsx
@@ -10,6 +10,15 @@ export default function Question({questionIndex, onSelectAnswer, onSkipAnswer}) 
         isCorrect: null,    
     });
 
+    let timer = 10000;
+
+    if (answer.selectedAnswer) {
+        timer = 1000;
+    }
+
+    if (answer.isCorrect !== null) {
+        timer = 2000;
+    }
     function handleSelectAnswer(answer) {
         setAnswer({
             selectedAnswer: answer,
@@ -35,7 +44,7 @@ export default function Question({questionIndex, onSelectAnswer, onSkipAnswer}) 
     }
     return (
         <div id="question">
-            <QuestionTimer timeout={10000} onTimeout={() => onSkipAnswer(null)} />
+            <QuestionTimer timeout={timer} onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null} mode={answerState} key={timer} />
             <h2>{QUESTIONS[questionIndex].text}</h2>
             <Answers
                 answers={QUESTIONS[questionIndex].answers} 
