b9d2ea24b663ae97ab6d5329f3df2d320947b8e6
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-09 17:57:21 +0200

    Example of using a context for a class based component

--- 12-class-based-components/src/components/UserFinder.js
+++ 12-class-based-components/src/components/UserFinder.js
@@ -2,14 +2,10 @@ import { Component, Fragment, useState, useEffect } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
-
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+import UsersContext from '../store/users-context'; // In class based components, you can listen to only one context
 
 class UserFinder extends Component {
+    static contextType = UsersContext
     constructor() {
         super();
         this.state = {
@@ -20,12 +16,12 @@ class UserFinder extends Component {
 
     componentDidMount() {
         // Send http request
-        this.setState({ filteredUsers: DUMMY_USERS });
+        this.setState({ filteredUsers: this.context.users });
     }
 
     componentDidUpdate(previousProps, previousState) {
         if (previousState.searchTerm !== this.state.searchTerm) {
-            this.setState({filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))});
+            this.setState({filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm))});
         }
     }
 
