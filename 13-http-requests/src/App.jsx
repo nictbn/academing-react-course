0a95eab48a36dec41f1cafe2837fa4bd40521744
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-11 00:46:19 +0200

    Finish app

--- 13-http-requests/src/App.jsx
+++ 13-http-requests/src/App.jsx
@@ -1,11 +1,11 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { updateUserPlaces } from './http.js';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
 import ErrorPage from './components/ErrorPage.jsx';
 
 function App() {
@@ -15,6 +15,23 @@ function App() {
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState(null);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch(error) {
+        setError({message: error.message || 'Failed to fetch user places.'});
+      }
+      setIsFetching(false);
+    }
+    fetchPlaces();
+  }, [])
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -83,12 +100,15 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
+        {error && <ErrorPage title="An error occurred" message={error.message}/>}
+        {!error && <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          isLoading={isFetching}
+          loadingText="Fetching your places..."
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
-        />
+        />}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
