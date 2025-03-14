fb10d878cf1da3d41601600e17cb07d740faa030
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-14 22:47:56 +0200

    Use one state object to handle all form inputs

--- 15-forms-user-input/src/components/Login.jsx
+++ 15-forms-user-input/src/components/Login.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 
 export default function Login() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('submitted');
+    console.log(enteredValues);
   }
 
-  function handleEmailChange(event) {
-    setEnteredEmail(event.target.value);
-  }
-
-  function handlePasswordChange(event) {
-    setEnteredPassword(event.target.value);
+  function handleInputChange(identifier, value) {
+    setEnteredValues(previousValues => ({
+      ...previousValues,
+      [identifier]: value
+    }))
   }
 
   return (
@@ -24,12 +25,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={handleEmailChange} value={enteredEmail}/>
+          <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={handlePasswordChange} value={enteredPassword}/>
+          <input id="password" type="password" name="password" onChange={(event) => handleInputChange('password', event.target.value)} value={enteredValues.password}/>
         </div>
       </div>
 
