4b792ca1739f8420ab352974f9ee72322051aac4
Nicu Taban <nictbndeveloper@gmail.com>
2025-03-14 22:38:14 +0200

    Handle input data using multiple states

--- 15-forms-user-input/src/components/Login.jsx
+++ 15-forms-user-input/src/components/Login.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log('submitted');
   }
+
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
+  }
+
   return (
     <form onSubmit={handleSubmit} >
       <h2>Login</h2>
@@ -10,12 +24,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={handleEmailChange} value={enteredEmail}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={handlePasswordChange} value={enteredPassword}/>
         </div>
       </div>
 
