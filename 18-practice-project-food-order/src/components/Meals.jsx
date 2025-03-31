2784abb388b02982214c4194a92d0da36a1e757f
Nicu Taban <nictbndeveloper@gmail.com>
2025-04-01 00:48:44 +0300

    Displays the list of meal items

--- 18-practice-project-food-order/src/components/Meals.jsx
+++ 18-practice-project-food-order/src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import MealItem from './MealItem.jsx';
 
 export default function Meals() {
     const [ loadedMeals, setLoadedMeals ] = useState([]);
@@ -25,7 +26,7 @@ export default function Meals() {
     
     return (
         <ul id="meals">
-            {loadedMeals.map(meal => <li key={meal.id}>{meal.name}</li>)}
+            {loadedMeals.map(meal => <MealItem key={meal.id} meal={meal}/>)}
         </ul>
     )
 }
