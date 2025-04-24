2b96d2fefb7857bbcd37c4b60e1dfa36aa68686c
Nicu Taban <nictbndeveloper@gmail.com>
2025-04-25 00:19:29 +0300

    Opening and closing the cart modal

--- 18-practice-project-food-order/src/components/Header.jsx
+++ 18-practice-project-food-order/src/components/Header.jsx
@@ -2,12 +2,20 @@ import Button from "./ui/Button.jsx";
 import logoImg from '../assets/logo.jpg';
 import { useContext } from "react";
 import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
     }, 0)
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
+
     return (
         <header id="main-header">
             <div id="title">
@@ -15,7 +23,9 @@ export default function Header() {
                 <h1>ReactFood</h1>
             </div>
             <nav>
-                <Button textOnly>Cart ({totalCartItems})</Button>
+                <Button textOnly onClick={handleShowCart}>
+                    Cart ({totalCartItems})
+                </Button>
             </nav>
             
         </header>
