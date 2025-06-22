b99506afb4f82ca11a6bd5d357f772f0db1637a4
Nicu Taban <nictbndeveloper@gmail.com>
2025-06-23 00:16:06 +0300

    Migrate to form actions

--- 18-practice-project-food-order/src/components/Checkout.jsx
+++ 18-practice-project-food-order/src/components/Checkout.jsx
@@ -33,12 +33,10 @@ export default function Checkout() {
         clearData();
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
+    async function checkoutAction(formData) {
         const customerData = Object.fromEntries(formData.entries());
 
-        sendRequest(JSON.stringify({
+        await sendRequest(JSON.stringify({
             order: {
                 items: cartCtx.items,
                 customer: customerData
@@ -68,7 +66,7 @@ export default function Checkout() {
     }
 
     return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-        <form onSubmit={handleSubmit}>
+        <form action={checkoutAction}>
             <h2>Checkout</h2>
             <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
             <Input label="Full Name" type="text" id="name"/>
