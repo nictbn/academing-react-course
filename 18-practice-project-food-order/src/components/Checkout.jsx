5a57dae60966485cf66c544f430873118146ef41
Nicu Taban <nictbndeveloper@gmail.com>
2025-06-22 23:30:44 +0300

    Handling loading and error states

--- 18-practice-project-food-order/src/components/Checkout.jsx
+++ 18-practice-project-food-order/src/components/Checkout.jsx
@@ -39,7 +39,7 @@ export default function Checkout() {
             <Input label="Full Name" type="text" id="name"/>
             <Input label="E-Mail Address" type="email" id="email"/>
             <Input label="Street" type="text" id="street"/>
-            <div class="control=row">
+            <div className="control-row">
                 <Input label="Postal Code" type="text" id="postal-code"/>
                 <Input label="City" type="text" id="city"/>
             </div>
--- 18-practice-project-food-order/src/components/Meals.jsx
+++ 18-practice-project-food-order/src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import useHttp from '../hooks/useHttp.js';
+import Error from './Error.jsx';
 import MealItem from './MealItem.jsx';
 
 const requestConfig = {};
@@ -6,7 +7,10 @@ const requestConfig = {};
 export default function Meals() {
     const {data: loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, []);
     if (isLoading) {
-        return <p>Fetching meals...</p>
+        return <p className='center'>Fetching meals...</p>
+    }
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error} />
     }
     return (
         <ul id="meals">
