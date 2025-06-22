d79653f855233d24ed809d84ac2c75e66b7c0e1b
Nicu Taban <nictbndeveloper@gmail.com>
2025-06-22 22:40:06 +0300

    Adds the submit functionality

--- 18-practice-project-food-order/src/components/Checkout.jsx
+++ 18-practice-project-food-order/src/components/Checkout.jsx
@@ -13,11 +13,30 @@ export default function Checkout() {
     function handleClose() {
         userProgressCtx.hideCheckout();
     }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const customerData = Object.fromEntries(formData.entries());
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData
+                }
+            })
+        });
+    }
+
     return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Checkout</h2>
             <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-            <Input label="Full Name" type="text" id="full-name"/>
+            <Input label="Full Name" type="text" id="name"/>
             <Input label="E-Mail Address" type="email" id="email"/>
             <Input label="Street" type="text" id="street"/>
             <div class="control=row">
