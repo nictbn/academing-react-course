1b38a276e29264b1681cbb8709d646f627f47736
Nicu Taban <nictbndeveloper@gmail.com>
2025-06-27 21:21:33 +0300

    Change app to use redux toolkit

--- 19-redux-intro/src/components/Counter.js
+++ 19-redux-intro/src/components/Counter.js
@@ -2,6 +2,7 @@ import { Component} from 'react';
 import { useSelector, useDispatch, connect } from 'react-redux';
 
 import classes from './Counter.module.css';
+import { counterActions } from '../store/index.js';
 
 const Counter = () => {
   const counter = useSelector(state => state.counter);
@@ -9,19 +10,19 @@ const Counter = () => {
 
   const dispatch = useDispatch();
   const toggleCounterHandler = () => {
-    dispatch({type: 'toggle'});
+    dispatch(counterActions.toggleCounter());
   };
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'});
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({type: 'increase', amount: 5});
+    dispatch(counterActions.increase(5)); // {type: some unique identifier, payload: 5}
   };
   
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
 
   return (
