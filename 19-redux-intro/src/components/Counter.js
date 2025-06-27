830ed3595939da9ffbb6548ebfa21bee69c693d8
Nicu Taban <nictbndeveloper@gmail.com>
2025-06-27 21:49:53 +0300

    Working with multiple slices

--- 19-redux-intro/src/components/Counter.js
+++ 19-redux-intro/src/components/Counter.js
@@ -5,8 +5,8 @@ import classes from './Counter.module.css';
 import { counterActions } from '../store/index.js';
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
 
   const dispatch = useDispatch();
   const toggleCounterHandler = () => {
