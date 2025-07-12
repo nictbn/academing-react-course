4f9675ba9b76b59d08a20ae031f17274a93a570e
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-12 11:55:20 +0300

    Dynamic link examples

--- 21-multi-page-application/src/App.js
+++ 21-multi-page-application/src/App.js
@@ -4,6 +4,7 @@ import RootLayout from './pages/Root.js';
 import HomePage from './pages/Home.js';
 import ProductsPage from './pages/Products.js';
 import ErrorPage from './pages/Error.js';
+import ProductDetailPage from './pages/ProductDetail.js';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
       children: [
         { path: '/',  element: <HomePage /> },
         { path: '/products',  element: <ProductsPage /> },
+        { path: '/products/:productId',  element: <ProductDetailPage /> },
       ]
     },
   ]);
