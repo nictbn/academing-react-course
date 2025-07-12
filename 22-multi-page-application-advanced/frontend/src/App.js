d4b40dd690fa0dddf1f00ad62b85eb91e56b67d0
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-12 14:49:16 +0300

    Solve all tasks

--- 22-multi-page-application-advanced/frontend/src/App.js
+++ 22-multi-page-application-advanced/frontend/src/App.js
@@ -19,9 +19,28 @@
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home.js';
+import EventsPage from './pages/Events.js';
+import EventDetailPage from './pages/EventDetail.js';
+import NewEventPage from './pages/NewEvent.js';
+import EditEventPage from './pages/EditEvent.js';
+import RootLayout from './pages/Root.js';
+import EventsRootLayout from './pages/EventsRoot.js';
 
+const router = createBrowserRouter([
+  { path: '/', element: <RootLayout />, children:[
+    { index: true, element: <HomePage /> },
+    { path: 'events', element: <EventsRootLayout />, children: [
+      { index: true, element: <EventsPage /> },
+      { path: ':eventId', element: <EventDetailPage /> },
+      { path: 'new', element: <NewEventPage /> },
+      { path: ':eventId/edit', element: <EditEventPage /> },
+    ]},
+  ]},
+])
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />
 }
 
 export default App;
---
+++ 22-multi-page-application-advanced/frontend/src/pages/EventsRoot.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import EventsNavigation from '../components/EventsNavigation.js';
+
+function EventsRootLayout() {
+    return <>
+        <EventsNavigation />
+        <Outlet />
+    </>
+}
+
+export default EventsRootLayout;
