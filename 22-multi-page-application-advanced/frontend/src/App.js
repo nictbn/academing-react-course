8a1dcd8220f5aaef092da44ae88b0e96bc905bec
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-13 00:50:50 +0300

    Move the events loader to the events page

--- 22-multi-page-application-advanced/frontend/src/App.js
+++ 22-multi-page-application-advanced/frontend/src/App.js
@@ -21,7 +21,7 @@
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/Home.js';
-import EventsPage from './pages/Events.js';
+import EventsPage, { loader as eventsLoader} from './pages/Events.js';
 import EventDetailPage from './pages/EventDetail.js';
 import NewEventPage from './pages/NewEvent.js';
 import EditEventPage from './pages/EditEvent.js';
@@ -32,15 +32,7 @@ const router = createBrowserRouter([
   { path: '/', element: <RootLayout />, children:[
     { index: true, element: <HomePage /> },
     { path: 'events', element: <EventsRootLayout />, children: [
-      { index: true, element: <EventsPage />, loader: async () => {
-        const response = await fetch('http://localhost:8080/events');
-        if (!response.ok) {
-          // ...
-        } else {
-          const resData = await response.json();
-          return resData.events;
-        }
-      }},
+      { index: true, element: <EventsPage />, loader: eventsLoader },
       { path: ':eventId', element: <EventDetailPage /> },
       { path: 'new', element: <NewEventPage /> },
       { path: ':eventId/edit', element: <EditEventPage /> },
