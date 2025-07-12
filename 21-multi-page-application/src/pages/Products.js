7b7ffc7ec204ff54abfdb41906daf8cfdc9822f6
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-12 12:27:02 +0300

    Convert navigation links to relative links

--- 21-multi-page-application/src/pages/Products.js
+++ 21-multi-page-application/src/pages/Products.js
@@ -4,9 +4,9 @@ function ProductsPage() {
     return <>
         <h1>The Products Page</h1>
         <ul>
-            <li><Link to="/products/1">Product 1</Link></li>
-            <li><Link to="/products/2">Product 2</Link></li>
-            <li><Link to="/products/3">Product 3</Link></li>
+            <li><Link to="1">Product 1</Link></li>
+            <li><Link to="2">Product 2</Link></li>
+            <li><Link to="3">Product 3</Link></li>
         </ul>
     </>
 }
