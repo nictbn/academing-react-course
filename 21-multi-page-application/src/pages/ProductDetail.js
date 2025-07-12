7cba4504e60e4cec50fd9627f6cf3816be96aced
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-12 12:32:54 +0300

    Implement back button that takes you back one level in the url, not the path

--- 21-multi-page-application/src/pages/ProductDetail.js
+++ 21-multi-page-application/src/pages/ProductDetail.js
@@ -6,7 +6,7 @@ function ProductDetailPage() {
     return <>
         <h1>Product Details!</h1>
         <p>{params.productId}</p>
-        <p><Link to="..">Back</Link></p>
+        <p><Link to=".." relative="path">Back</Link></p>
     </>
 }
 
