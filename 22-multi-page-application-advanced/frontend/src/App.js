1c6b6757d3075ff20cdb6c2f5fa7aad055181cec
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-13 11:32:12 +0300

    View event details

--- 22-multi-page-application-advanced/frontend/src/App.js
+++ 22-multi-page-application-advanced/frontend/src/App.js
@@ -22,7 +22,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/Home.js';
 import EventsPage, { loader as eventsLoader} from './pages/Events.js';
-import EventDetailPage from './pages/EventDetail.js';
+import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail.js';
 import NewEventPage from './pages/NewEvent.js';
 import EditEventPage from './pages/EditEvent.js';
 import RootLayout from './pages/Root.js';
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
     { index: true, element: <HomePage /> },
     { path: 'events', element: <EventsRootLayout />, children: [
       { index: true, element: <EventsPage />, loader: eventsLoader },
-      { path: ':eventId', element: <EventDetailPage /> },
+      { path: ':eventId', element: <EventDetailPage />, loader: eventDetailLoader },
       { path: 'new', element: <NewEventPage /> },
       { path: ':eventId/edit', element: <EditEventPage /> },
     ]},
