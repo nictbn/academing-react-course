e80c171580dd014c94b3b6abde0a48f4fef3328d
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-13 10:43:57 +0300

    Minor change

--- 22-multi-page-application-advanced/frontend/src/pages/Events.js
+++ 22-multi-page-application-advanced/frontend/src/pages/Events.js
@@ -3,7 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-    const events = useLoaderData();
+    const data = useLoaderData();
+    const events = data.events;
   return (
     <>
       <EventsList events={events}/>
@@ -19,6 +20,6 @@ export async function loader() {
         // ...
     } else {
         const resData = await response.json();
-        return resData.events;
+        return resData;
     }
 }
