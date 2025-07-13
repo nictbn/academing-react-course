c88848e9d6f57f077bdd2601efb292e5000db912
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-13 10:47:47 +0300

    One way of handling errorss

--- 22-multi-page-application-advanced/frontend/src/pages/Events.js
+++ 22-multi-page-application-advanced/frontend/src/pages/Events.js
@@ -4,6 +4,9 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
     const data = useLoaderData();
+    if (data.isError) {
+      return <p>{data.message}</p>;
+    }
     const events = data.events;
   return (
     <>
@@ -17,7 +20,7 @@ export default EventsPage;
 export async function loader() {
     const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
-        // ...
+      return { isError: true, message: 'Could not fetch events.' };
     } else {
         const resData = await response.json();
         return resData;
