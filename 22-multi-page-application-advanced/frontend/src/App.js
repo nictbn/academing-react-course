086ef35a7a7fcc1f1002261778962211ec6d8cdd
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-13 11:48:04 +0300

    Reuse loader across multiple routes

--- 22-multi-page-application-advanced/frontend/src/App.js
+++ 22-multi-page-application-advanced/frontend/src/App.js
@@ -34,9 +34,14 @@ const router = createBrowserRouter([
     { index: true, element: <HomePage /> },
     { path: 'events', element: <EventsRootLayout />, children: [
       { index: true, element: <EventsPage />, loader: eventsLoader },
-      { path: ':eventId', element: <EventDetailPage />, loader: eventDetailLoader },
+      { path: ':eventId',
+        loader: eventDetailLoader,
+        id: 'event-detail',
+        children: [
+        { index: true, element: <EventDetailPage /> },
+        { path: 'edit', element: <EditEventPage /> },
+      ]},
       { path: 'new', element: <NewEventPage /> },
-      { path: ':eventId/edit', element: <EditEventPage /> },
     ]},
   ]},
 ])
