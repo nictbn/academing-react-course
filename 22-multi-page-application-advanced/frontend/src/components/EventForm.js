8225b282a5f73dcb2e450624cfc471ca5710c84d
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-19 12:02:02 +0300

    Manage submitting state

--- 22-multi-page-application-advanced/frontend/src/components/EventForm.js
+++ 22-multi-page-application-advanced/frontend/src/components/EventForm.js
@@ -1,13 +1,15 @@
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
   function cancelHandler() {
     navigate('..');
   }
-
+  
+  const isSubmitting = navigation.state === 'submitting';
   return (
     <Form method='post' className={classes.form}>
       <p>
@@ -27,10 +29,10 @@ function EventForm({ method, event }) {
         <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''}/>
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting ? 'Submitting' : 'Save'}</button>
       </div>
     </Form>
   );
