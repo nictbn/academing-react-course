995b10ecb68c512570e400da17d01f31b16da8aa
Nicu Taban <nictbndeveloper@gmail.com>
2025-07-29 22:51:52 +0300

    Add automatic expiration of token

--- 23-authentication/frontend/src/pages/Root.js
+++ 23-authentication/frontend/src/pages/Root.js
@@ -1,8 +1,27 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post'});
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post'});
+    }, tokenDuration);
+  }, [token, submit]);
   // const navigation = useNavigation();
 
   return (
