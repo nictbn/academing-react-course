3ec3b87f97dc91625f4a30a22dc904c54542ccdf
Nicu Taban <nictbndeveloper@gmail.com>
2025-08-06 00:14:05 +0300

    Use an event router action to edit an event

--- 25-tanstack-query/src/components/Events/EditEvent.jsx
+++ 25-tanstack-query/src/components/Events/EditEvent.jsx
@@ -1,5 +1,5 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { Link, redirect, useNavigate, useParams, useSubmit } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
@@ -8,33 +8,15 @@ import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const submit = useSubmit();
   const params = useParams();
   const {data, isError, error } = useQuery({
     queryKey: ['events', params.id],
     queryFn: ({signal}) => fetchEvent({signal, id: params.id})
   })
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (data) => {
-      const newEvent = data.event;
-
-      await queryClient.cancelQueries({queryKey: ['events', params.id]});
-      const previousEvent = queryClient.getQueryData(['events', params.id]);
-      queryClient.setQueryData(['events', params.id], newEvent);
-      return { previousEvent }
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(['events', params.id], context.previousEvent );
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(['events', params.id]);
-    }
-  })
-
   function handleSubmit(formData) {
-    mutate({ id: params.id, event: formData });
-    navigate('../');
+    submit(formData, {method: 'PUT'}) //  not sending an http request, just triggering the action
   }
 
   function handleClose() {
@@ -77,4 +59,12 @@ export function loader({params}) {
     queryKey: ['events', params.id],
     queryFn: ({signal}) => fetchEvent({signal, id: params.id})
   })
+}
+
+export async function action({request, params}) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({id: params.id, event: updatedEventData});
+  await queryClient.invalidateQueries(['events']);
+  return redirect('../');
 }
