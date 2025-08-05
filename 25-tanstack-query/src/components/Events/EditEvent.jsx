181774ebaa4e40a0e6b90862ff75dda23007f962
Nicu Taban <nictbndeveloper@gmail.com>
2025-08-06 00:05:21 +0300

    Combine react router and react query

--- 25-tanstack-query/src/components/Events/EditEvent.jsx
+++ 25-tanstack-query/src/components/Events/EditEvent.jsx
@@ -4,13 +4,12 @@ import { useQuery, useMutation } from '@tanstack/react-query';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
-import LoadingIndicator from '../UI/LoadingIndicator.jsx'
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
   const params = useParams();
-  const {data, isPending, isError, error } = useQuery({
+  const {data, isError, error } = useQuery({
     queryKey: ['events', params.id],
     queryFn: ({signal}) => fetchEvent({signal, id: params.id})
   })
@@ -44,12 +43,6 @@ export default function EditEvent() {
 
   let content;
 
-  if (isPending) {
-    content = <div className='center'>
-      <LoadingIndicator />
-    </div> 
-  }
-
   if (isError) {
     content = <>
       <ErrorBlock title="Failed to load event" message={error.info?.message || 'Failed to load event. Please check your inputs and try again later.'}/>
@@ -77,4 +70,11 @@ export default function EditEvent() {
       {content}
     </Modal>
   );
+}
+
+export function loader({params}) {
+  return queryClient.fetchQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) => fetchEvent({signal, id: params.id})
+  })
 }
