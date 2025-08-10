19cb64c901688deb36003d20ce796fa1e206260e
Nicu Taban <nictbndeveloper@gmail.com>
2025-08-10 23:29:02 +0300

    Add error page

--- 27-nextjs-foodies-app/app/lib/meals.js
+++ 27-nextjs-foodies-app/app/lib/meals.js
@@ -5,5 +5,7 @@ const db = sql('meals.db');
 // async is just for demo purposes, the driver is not async
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 2000));
+    // Uncomment to simulate an error
+    // throw new Error('Loading meals failed');
     return db.prepare('SELECT * FROM meals').all();
 }
