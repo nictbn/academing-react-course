29aafe459ec33a39489ee20ce1510dc7fa994a17
Nicu Taban <nictbndeveloper@gmail.com>
2025-08-10 23:11:31 +0300

    Display meals from the database

---
+++ 27-nextjs-foodies-app/app/lib/meals.js
@@ -0,0 +1,9 @@
+import sql from 'better-sqlite3';
+
+const db = sql('meals.db');
+
+// async is just for demo purposes, the driver is not async
+export async function getMeals() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return db.prepare('SELECT * FROM meals').all();
+}
