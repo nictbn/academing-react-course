86b3e4740cc24487d90794b44f46204cc78a99b9
Nicu Taban <nictbndeveloper@gmail.com>
2025-08-11 21:36:16 +0300

    Display single meal

--- 27-nextjs-foodies-app/app/meals/[meal]/page.js
+++ 27-nextjs-foodies-app/app/meals/[meal]/page.js
@@ -1,5 +1,29 @@
-export default function MealDetailsPage() {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/app/lib/meals';
+
+export default function MealDetailsPage({params}) {
+    const meal = getMeal(params.meal);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
     return (
-        <h1>Meal Details</h1>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                </div>
+                <p className={classes.summary}>{meal.summary}</p>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}></p>
+            </main>
+        </>
     )
 }
