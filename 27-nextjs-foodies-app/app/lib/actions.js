acdf11174c663a505ab5b944dd16496979fe909b
Nicu Taban <nictbndeveloper@gmail.com>
2025-08-17 22:58:13 +0300

    Add share meal functionality

--- 27-nextjs-foodies-app/app/lib/actions.js
+++ 27-nextjs-foodies-app/app/lib/actions.js
@@ -1,5 +1,7 @@
 'use server'
 
+import { saveMeal } from "./meals"
+
 export async function shareMeal(formData) {
     const meal = {
         title: formData.get('title'),
@@ -9,4 +11,5 @@ export async function shareMeal(formData) {
         creator: formData.get('name'),
         creator_email: formData.get('email'),
     }
+    await saveMeal(meal);
 }
